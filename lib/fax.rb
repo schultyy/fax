d5a60e78b5ddcabb822c9940fc2cd0f1dc006710
schultyy <janschulte90@googlemail.com>
2013-10-20 22:48:51 +0200

    all get methods deliver json

--- lib/fax.rb
+++ lib/fax.rb
@@ -3,18 +3,18 @@ require 'sinatra/contrib'
 require 'fax_geraet'
 
 get '/folders' do
-  fetch_folders
+  json fetch_folders
 end
 
 get '/:folder' do
   folder_name = params[:folder]
-  show_folder_content(folder_name)
+  json show_folder_content(folder_name)
 end
 
 get '/:folder/mail/:id' do
   folder_name = params[:folder]
   mail_id = params[:id]
-  get_mail_by_id(folder_name, mail_id)
+  json get_mail_by_id(folder_name, mail_id)
 end
 
 post '/sendmail' do
