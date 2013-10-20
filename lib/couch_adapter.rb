ed55ac8664a3a91d77995d2660a049f8840d02cd
schultyy <janschulte90@googlemail.com>
2013-10-20 23:17:44 +0200

    can include_docs

--- lib/couch_adapter.rb
+++ lib/couch_adapter.rb
@@ -5,8 +5,10 @@ class CouchAdapter
     @host = host
     @database = database
   end
-  def query_view(design, view_name)
-    request("GET", "_design/#{design}/_view/#{view_name}")
+  def query_view(design, view_name, include_docs)
+    query = "_design/#{design}/_view/#{view_name}"
+    query << "?include_docs=true" if include_docs
+    request("GET", query)
   end
   private
   def request(method, actual_request)
