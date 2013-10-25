514a9f30e4b7aede9065622d65e410d3abc6fda8
schultyy <janschulte90@googlemail.com>
2013-10-25 22:45:51 +0200

    Added mocks and first testcase for GET /folders

--- spec/fax_spec.rb
+++ spec/fax_spec.rb
@@ -1,4 +1,5 @@
 require './spec/spec_helper'
+require './lib/faxgeraet'
 
 describe 'The fax app' do
   include Rack::Test::Methods
@@ -7,9 +8,23 @@ describe 'The fax app' do
     Sinatra::Application
   end
 
+  before :each do
+    @fax = double("Fax::Faxgeraet")
+  end
+
   it "says hello" do
     get '/'
     expect(last_response).to be_ok
     expect(last_response.body).to eq('"hello"')
+  end
+  context 'Folders' do
+    before do
+      Fax::Faxgeraet.stub(:new).with(any_args).and_return(@fax)
+      @fax.stub(:fetch_folders).with(any_args).and_return([])
+    end
+    it '/folders should return all folders' do
+      get '/folders'
+      expect(last_response).to be_ok
+    end
   end
 end
